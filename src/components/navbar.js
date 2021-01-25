8f962851fa5574aa14e4bc3db2655e8286d46d27
osamahabib5 <osama.habib@khi.iba.edu.pk>
2021-01-25 11:13:44 +0500

    Starting Navigation using Bootstrap

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,59 +1,45 @@
-import React ,{useState}from 'react'
-import{Link} from 'react-router-dom';
-import {FaBars, FaTimes} from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import logo from '../logo/Logo.png'
 import './navbar.css'
-function Navbar() {
-
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-    
-    return (
-        <div className = "navbar">
-            <div className = "navbar-container container">
-                <Link to = "/" className = "navbar-logo">
-                         <img src = {logo} alt = "Logo"/>
-                    </Link>
-                    <div className='menu-icon' onClick={handleClick}>
-              {click ? <FaTimes /> : <FaBars />}
-            </div>
-
-                    {/* Navigation Bar */}
-                    <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className = "nav-item">
-                            <Link to = "/" className = "nav-links">
-                                Programs & Courses
-                            </Link>
-                        </li>
-                        <li className = "nav-item">
-                            <Link to = "/" className = "nav-links">
-                                Pricing
-                            </Link>
-                        </li>
-                        <li className = "nav-item">
-                            <Link to = "/" className = "nav-links">
-                                Our Tutors
-                            </Link>
-                        </li>
-                        <li className = "nav-item">
-                            <Link to = "/" className = "nav-links">
-                                Become a Tutor
-                            </Link>
-                        </li>
-                        <li className = "nav-item">
-                            <Link to = "/" className = "nav-links">
-                                About Our Company
-                            </Link>
-                        </li>
-                        <li className = "nav-item">
-                            <Link to = "/" className = "nav-links">
-                                Contact Us
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>         
-    )
+function Navbar(props) {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  return (
+    <header className="toolbar">
+      <nav className="toolbar__navigation">
+        <div className='menu-icon' onClick={handleClick}>
+          {click ? <FaTimes /> : <FaBars />}
+        </div>
+        <div className="toolbar__logo">
+          <img src={logo} alt="logo" color="black" />
+        </div>
+        <div className="spacer" />
+        <div className="toolbar_navigation-items">
+          <ul>
+            <li>
+              <a href="/">Programs and Courses</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+            <li>
+              <a href="/">Our Tutors</a>
+            </li>
+            <li>
+              <a href="/">Become a Tutor</a>
+            </li>
+            <li>
+              <a href="/">About Our Company</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Navbar
---
+++ src/components/sidedrawer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './sidedrawer.css';
+function Sidedrawer() {
+    return (
+        <nav className="side-drawer">
+        <ul>
+          <li>
+            <a href="/">Programs and Courses</a>
+          </li>
+          <li>
+            <a href="/">Pricing</a>
+          </li>
+          <li>
+            <a href="/">Our Tutors</a>
+          </li>
+          <li>
+            <a href="/">Become a Tutor</a>
+          </li>
+          <li>
+            <a href="/">About Our Company</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    )
+}
+
+export default Sidedrawer
